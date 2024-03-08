8a1a1d9c64d84be33898233472fd004363766c40
Ranjeet Dethe <130579549+ranjeetdethe@users.noreply.github.com>
2024-03-08 22:32:18 +0530

    Add files via upload

---
+++ Program4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class InputDemo{
+        public static void main(String[]args)throws IOException{
+                BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("Enter num ");
+                int num=Integer.parseInt(br.readLine());
+
+                int count=0;
+                int i=1;
+                while(i<=num){
+                        if(num%i==0){
+                                count++;
+                        }
+                        i++;
+                }
+                if(count>2){
+                        System.out.println(num +" is a composite number");
+                }else{
+                        System.out.println(num +" is not a composite prime number");
+                }
+        }
+}
